7dcb871ea143dd146bd822a2fe87876a7b456e9c
unknown <patrickschuermann1997@gmail.com>
2023-02-28 17:00:28 +0100

    started polynomial addition function

--- miura/src/poly.rs
+++ miura/src/poly.rs
@@ -1,3 +1,5 @@
+use std::cmp::max; // maximum function
+
 /*
 * Models a polynomial with either integer coefficients 
 * or coefficients from a remainder class ring. 
@@ -80,11 +82,71 @@ impl IntPoly {
 }
 
 /*
+* Returns the sum of the two passed polynomials.
+* Trailing zeros of the sum are cut in the process.
+*
+* If the moduli of the polynomials do not match, the function returns an error. 
+*/
+pub fn add_poly(poly1: &IntPoly, poly2: &IntPoly) -> Result<IntPoly, PolynomialError> {
+    // two polynomials with non-matching moduli cannot be added meaningfully
+    if poly1.modulus != poly2.modulus {
+        return Err(
+            PolynomialError::ModulusMismatchError(poly1.modulus, poly2.modulus)
+        );
+    }
+    
+    let result_len = max(poly1.deg(), poly2.deg());
+
+    let mut result_coeffs = vec![];
+
+    // computing the coefficients
+    for i in 0..result_len {
+        result_coeffs.push(poly1.coefficient(i) + poly2.coefficient(i));
+    }
+
+    // creating result polynomial 
+    let result_poly = IntPoly::new(
+        &mut result_coeffs,
+        poly1.modulus // at this point, the polynomials can be assumed to have the same modulus
+    );
+
+    /*
+    * Remove trailing zeros from the result polynomial.
+    * 
+    * When adding e.g. X^2 + 1 and -X^2 + 1 over Z, the sum is 2 
+    * and has a lower degree than the summands.
+    *
+    * When adding e.g. 3X^2 + X and 2X^2 + 2X over Z/5Z, 
+    * the sum is 5X^2 + 3X = 3X and has a lower degree than the summands.
+    */
+
+    Ok(result_poly)
+}
+
+/*
+* Removes the trailing zeros from the passed vector,
+* e.g. vec![2, 3, 0, 0] becomes vec![2, 3].
+*/
+fn remove_trailing_zeros(vec: &mut Vec<i32>) {
+
+}
+
+/*
 * A modulus for a remainder class ring.
 * Implementation for the binary equals-operator is generated automatically using derived traits.
 */
-#[derive(PartialEq)]
+#[derive(PartialEq, Copy, Clone)]
 pub enum Modulus {
     Some(i32),
     None
+}
+
+/*
+* Models the different error types that can occur when working with polynomials.
+*/
+pub enum PolynomialError {
+    /*
+    * Returned when trying to do some binary operation for polynomials with different moduli.
+    */
+    ModulusMismatchError(Modulus, Modulus)
 }
--- miura/target/debug/deps/miura-7705b4d8c39059c4.d
+++ miura/target/debug/deps/miura-7705b4d8c39059c4.d
@@ -1,5 +1,6 @@
-C:\Users\Patrick\Documents\Rust\projects\miura\miura\target\debug\deps\miura-7705b4d8c39059c4.rmeta: src\lib.rs
+C:\Users\Patrick\Documents\Rust\projects\miura\miura\target\debug\deps\miura-7705b4d8c39059c4.rmeta: src\lib.rs src\poly.rs
 
-C:\Users\Patrick\Documents\Rust\projects\miura\miura\target\debug\deps\miura-7705b4d8c39059c4.d: src\lib.rs
+C:\Users\Patrick\Documents\Rust\projects\miura\miura\target\debug\deps\miura-7705b4d8c39059c4.d: src\lib.rs src\poly.rs
 
 src\lib.rs:
+src\poly.rs:
